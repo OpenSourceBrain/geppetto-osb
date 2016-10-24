60629bf5d56685084f7187025426684ad30ace69
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-10-24 17:21:23 +0100

    Fix for tutorial

--- ComponentsInitialization.js
+++ ComponentsInitialization.js
@@ -1,34 +1,3 @@
-/*******************************************************************************
- *
- * Copyright (c) 2011, 2016 OpenWorm.
- * http://openworm.org
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the MIT License
- * which accompanies this distribution, and is available at
- * http://opensource.org/licenses/MIT
- *
- * Contributors:
- *      OpenWorm - http://openworm.org/people.html
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
- *******************************************************************************/
 define(function(require) {
 
     return function(GEPPETTO) {
@@ -144,11 +113,9 @@ define(function(require) {
 
         //Tutorial component initialization
         //TODO Change this so that the URL is passed as a proprty inside the {}
-        GEPPETTO.ComponentFactory.addComponent('TUTORIAL', {}, document.getElementById("tutorial"),
-            function() {
-                GEPPETTO.Tutorial.setTutorial(Events.Experiment_loaded, "https://dl.dropboxusercontent.com/s/puwpjdy9u7bfm2s/osb_tutorial.json?dl=1");
-                GEPPETTO.Tutorial.tutorialData("https://dl.dropboxusercontent.com/s/puwpjdy9u7bfm2s/osb_tutorial.json?dl=1");
-            });
+        GEPPETTO.ComponentFactory.addComponent('TUTORIAL', {
+        	tutorial: "https://dl.dropboxusercontent.com/s/puwpjdy9u7bfm2s/osb_tutorial.json?dl=1"
+		}, document.getElementById("tutorial"));
 
         //Loading spinner initialization
         GEPPETTO.Spinner.setLogo("gpt-osb");
