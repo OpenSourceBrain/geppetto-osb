f09b012bfa2155fade66470e78713ac38db34e36
johnidol <giovanni.idili@gmail.com>
2017-01-17 13:02:54 +0000

    formatting

--- ComponentsInitialization.js
+++ ComponentsInitialization.js
@@ -880,7 +880,6 @@ define(function(require) {
 
 
         //OSB Geppetto events handling
-
         GEPPETTO.on(Events.Model_loaded, function() {
             if (Model.neuroml != undefined && Model.neuroml.importTypes != undefined && Model.neuroml.importTypes.length > 0) {
                 $('#mainContainer').append('<div class="alert alert-warning osb-notification alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><span class="osb-notification-text">' + Model.neuroml.importTypes.length + ' projections in this model have not been loaded yet. <a href="javascript:loadConnections();" class="alert-link">Click here to load the connections.</a> (Note: depending on the size of the network this could take some time).</span></div>');
@@ -890,11 +889,14 @@ define(function(require) {
         GEPPETTO.on(Events.Project_loading, function() {
             $('.osb-notification').remove();
         });
-        
+
+        GEPPETTO.on(Events.Experiment_loaded, function() {
+            // reset control panel with defaults
+            resetControlPanel(instancesCols, instancesColumnMeta, instancesControls, instancesControlsConfiguration);
+        });
 
 
         //OSB Utility functions
-        
         window.loadConnections = function() {
             Model.neuroml.resolveAllImportTypes(function() {
                 $(".osb-notification-text").html(Model.neuroml.importTypes.length + " projections and " + Model.neuroml.connection.getVariableReferences().length + " connections were successfully loaded.");
