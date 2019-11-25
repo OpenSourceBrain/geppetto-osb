37553c4cd6186ecc109fab84b7a7a6957f61d7fe
Matt Earnshaw <matt@earnshaw.org.uk>
2019-11-25 16:30:16 +0000

    update lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,18 @@
 module.exports = {
-  extends: [
-    "./node_modules/@geppettoengine/geppetto-client/.eslintrc.js",
-  ],
+	  extends: [
+		      "./node_modules/@geppettoengine/geppetto-client/.eslintrc.js",
+		      "plugin:jest/recommended"
+		    ],
+	  plugins: ["jest"],
+	  globals: {
+		      page: true,
+		      browser: true,
+		      context: true,
+		      jestPuppeteer: true,
+		      acnet2: true,
+		      c302: true,
+		      pvdr: true,
+		      net1: true,
+		    },
+
 };
